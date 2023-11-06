1a6e41e99ee1a61f6aca469cd42c47ff2f8f406b
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-11-06 11:33:09 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -6,16 +6,21 @@ characters = "abcdefghijklmnopqrstuvwxyz0123456789"
 # 用于存储满足条件的数据
 output_data = []
 
-# 生成所有可能的组合
-combinations = itertools.product(characters, repeat=4)
+def generate_combinations(prefix, length):
+    if length == 0:
+        output_data.append(prefix)
+        return
+    for char in characters:
+        generate_combinations(prefix + char, length - 1)
 
-for combo in combinations:
-    combo_str = ''.join(combo)
-    if (combo_str[0] == 'a' and combo_str[1:].count('b') == 3) or (combo_str[0] == 'a' and combo_str[1:].count('a') == 3):
-        output_data.append(combo_str)
+# 生成所有可能的组合
+generate_combinations("", 4)
+
+# 过滤并保存符合条件的组合
+filtered_data = [combo for combo in output_data if (combo[0] == 'a' and combo[1:].count('b') == 3) or (combo[0] == 'a' and combo[1:].count('a') == 3)]
 
 # 输出数据到文件
 with open("4l.txt", "w") as file:
-    file.write("\n".join(output_data))
+    file.write("\n".join(filtered_data))
 
-print(f"已生成并保存 {len(output_data)} 条数据到 4l.txt 文件。")
+print(f"已生成并保存 {len(filtered_data)} 条数据到 4l.txt 文件。")
