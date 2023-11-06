10eb79efed0650fe6af32e4cc0e01448ace95dea
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-11-06 11:42:31 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,17 +1,17 @@
 import itertools
 
-# 生成所有可能的组合
-combinations = []
-for first_char in 'abcdefghijklmnopqrstuvwxyz':
-    for char_2 in 'abcdefghijklmnopqrstuvwxyz0123456789':
-        for char_3 in 'abcdefghijklmnopqrstuvwxyz0123456789':
-            for char_4 in 'abcdefghijklmnopqrstuvwxyz0123456789':
-                combination = first_char + char_2 + char_3 + char_4
-                combinations.append(combination)
+def generate_combinations():
+    for first_char in 'abcdefghijklmnopqrstuvwxyz':
+        for second_char in 'abcdefghijklmnopqrstuvwxyz0123456789':
+            for third_char in 'abcdefghijklmnopqrstuvwxyz0123456789':
+                for fourth_char in 'abcdefghijklmnopqrstuvwxyz0123456789':
+                    yield f"{first_char}{second_char}{third_char}{fourth_char}"
 
-# 过滤出符合条件的组合
-filtered_combinations = [combo for combo in combinations if any(combo[i] == combo[i+1] for i in range(3))]
+def main():
+    with open('4l.txt', 'w') as file:
+        for combination in generate_combinations():
+            if combination[1:4] == combination[0] * 3:
+                file.write(combination + '\n')
 
-# 将符合条件的组合保存到文件
-with open('4l.txt', 'w') as file:
-    file.write('\n'.join(filtered_combinations))
+if __name__ == '__main__':
+    main()
